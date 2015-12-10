7414e3e6f0d7492033fece65ca2d2b3b66283935
Yu-Cheng Lin <r99525067@ntu.edu.tw>
2015-12-10 14:09:11 +0800

    test
    test

---
+++ New Unity Project 2/Assets/01/cube_test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cube_test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("This is :"+ this.name);
+		Debug.Log ("The other is :"+ other.name); 
+	}
+}
---
+++ New Unity Project 2/Assets/hit/CreateBall.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateBall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (Input.mousePosition);
+
+		// Create ballet dynamically with left mouse key
+		// Input.GetMouseButtonDown(0)//left
+		if(Input.GetKeyDown(KeyCode.Mouse0)){
+			//Debug.Log("This is left mouse click: " + this.name);
+			GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			sphere.transform.position = this.transform.position;
+		}
+	}
+}
